348d6126ab551d5ccc3145c86976724ed87ac487
Baddo <paradisecity93@gmail.com>
2020-05-27 00:52:32 +0200

    aggiunto roll of favorite

--- src/app/favorites/favorites.page.ts
+++ src/app/favorites/favorites.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Macro } from '../Class/macro-class/macro';
+import { ToastController } from '@ionic/angular';
+import { first } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-favorites',
@@ -10,9 +13,11 @@ import { Macro } from '../Class/macro-class/macro';
 export class FavoritesPage {
 
   Macros:Macro[]=[];
+  resultToastPresent=new Subject();
 
   constructor(
-    private storage:Storage
+    private storage:Storage,
+    private toastController:ToastController
   ) { }
 
   ionViewWillEnter(){
@@ -27,5 +32,45 @@ export class FavoritesPage {
 
   openMacro(id){
     console.log(id);
+  }
+
+  async Roll(roll){
+    roll.reRoll();
+    if(roll.isValid){
+      const toast = await this.toastController.create({
+        header: 'Risultato: '+roll.Result,
+        position: 'top',
+        duration: 3000,
+        buttons: [
+          {
+            text: '',
+            side: 'end',
+            role: 'cancel',
+            icon:'close',
+            handler: () => {
+              console.log('Cancel clicked');
+            }
+          }
+        ]
+      });
+      this.resultToastPresent.next();
+      this.resultToastPresent.pipe(first()).subscribe(()=>{
+        toast.dismiss();
+      });
+      this.storage.set("log"+Date.now(),JSON.stringify(roll.getLog())).then(()=>{});
+      this.storage.keys().then((list)=>{
+        list
+        .filter((el)=>String(el).startsWith("log"))
+        .sort((a, b) => (Number(String(a).substr(3)) > Number(String(b).substr(3)) ? -1 : 1))
+        .slice(20)
+        .forEach((key)=>{
+          this.storage.remove(key);
+        })
+      })
+      .catch((err)=>{
+        console.log("err",err,roll.getLog());
+      });
+      toast.present();
+    }
   }
 }
